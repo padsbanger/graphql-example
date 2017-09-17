c5ad799e81a6d7de886233814b67ee11e4dc2af8
Michał Lach <kontakt@michal-lach.pl>
2017-09-17 16:37:55 +0200

    schema

---
+++ schema/schema.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString},
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt}
+  }
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(data => data.data);
+      }
+    }
+  }
+});
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
